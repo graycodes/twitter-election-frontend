c17bb59dfb4f6dcc823f5cfa606632600f0d053e
Gray MacGregor <gmacg.uk@gmail.com>
2015-05-04 14:01:02 +0100

    Yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twitterElectionFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the twitterElectionFrontendApp
+ */
+angular.module('twitterElectionFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
